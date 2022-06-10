a1d299e9f95c31354523ccd5eda5e5589d7460b1
nhanlnt <nhanlnt@smartosc.com>
2022-06-10 17:34:21 +0700

    [integrate-api/profile-page]: hide unused item of navigation

--- client/src/components/navigation/components/NavigationMenu.tsx
+++ client/src/components/navigation/components/NavigationMenu.tsx
@@ -7,19 +7,22 @@ import { Box, Button } from '@mui/material';
 export const NavigationMenu: FC = () => {
   return (
     <Box className="navigation-menu">
-      {navigationMenu.map((val: INavigationMenu) => (
-        <Button
-          component={Link}
-          to="/"
-          key={val.label}
-          disabled={!val.status}
-          className="navigation-menu-item"
-          variant="text"
-        >
-          <img src={val.icon} alt="navigation-menu" />
-          <span>{val.label}</span>
-        </Button>
-      ))}
+      {navigationMenu.map(
+        (val: INavigationMenu) =>
+          val.status && (
+            <Button
+              component={Link}
+              to="/"
+              key={val.label}
+              disabled={!val.status}
+              className="navigation-menu-item"
+              variant="text"
+            >
+              <img src={val.icon} alt="navigation-menu" />
+              <span>{val.label}</span>
+            </Button>
+          ),
+      )}
     </Box>
   );
 };
